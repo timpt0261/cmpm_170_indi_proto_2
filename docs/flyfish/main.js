268ba456ae6f7a77a99129159dcfb32d374f5648
timpt0261 <rchavez0215@gmail.com>
2023-11-13 02:39:04 -0800

    Added Caharacter to Scene

--- docs/flyfish/main.js
+++ docs/flyfish/main.js
@@ -1,41 +1,81 @@
-title = "Fly Fish";
-
-description = `  
+title = "Fly Squid";
+// note changing the description changes the sfx
+description = `
+  [Hold] up
+  [Release] down
 `;
 
 characters = [
-'rrr'
+`
+
+b  bb
+ b1bbb
+b  bb
+
+`,
 ];
 
-options = {
-  isPlayingBgm: true,
-  isReplayEnabled: true,
-  seed: 2002,
+const G = {
+  WIDTH: 100,
+  HEIGHT: 300,
+  PARTICLE_SPEED_MIN: 0.5,
+  PARTICLE_SPEED_MAX: 1.0,
+  STOP_HEIGHT: 40,
 };
 
-const G = {
-	WIDTH: 100,
-	HEIGHT: 150,
-}
+/**
+* @typedef {{
+  * pos: Vector,
+  * speed: number
+  * }} Fish
+  */
+
+/**
+* @type  { Fish }
+*/
+let player;
+
+
+/**
+* @type  { Fish [] }
+*/
+let fish;
+
+
+
+options = {
+  viewSize:{x:G.HEIGHT, y:G.WIDTH},
+  // theme: "pixel",
+  isPlayingBgm: true,
+  isReplayEnabled: true,
+  seed: 2003
+};
+
 
 let horizontalVelocity = 0;
-let verticalLevel = 0;
-let pin;
-let player;
-let obstacles;
-let counter = 0;
-let rot = 0;
-let turnSpeed = 0.01
+let verticalVelocity = 0;
 
+
+let lines = [];
+let timer = 60;
+
+// Example usage: restart the game when a key is pressed
+// if (input.isJustPressed) {
+//   initializeGame();
+// }
 
 function update() {
   if (!ticks) {
+    player = {
+      pos: vec(G.HEIGHT* 0.1, G.WIDTH * 0.5),
+      speed: G.PARTICLE_SPEED_MIN
+    };
   }
 
-  player = {
-    pos:vec(G.WIDTH, G.HEIGHT),
-    angle:0,
-  }
-  player.pos = vec(G.WIDTH * 0.5 + horizontalVelocity, (G.HEIGHT - 60) + verticalLevel);
-  char("a", player.pos, {rotation: rot});
+  char("a", player.pos);
+  // handleCollsion();
+}
+
+function handleCollsion() {
+
 }
