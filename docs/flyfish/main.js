b4d210115b9a089e0b6eb9990cf192b3ac2fd81f
timpt0261 <rchavez0215@gmail.com>
2023-11-17 02:03:47 -0800

    Colliding with Fish increases school

--- docs/flyfish/main.js
+++ docs/flyfish/main.js
@@ -6,13 +6,13 @@ description = `
 
 characters = [
   `
-b  bb
- b1bbb
-b  bb
-`,`
 r  rr
  r1rrr
 r  rr
+`, `
+g  gg
+ g1ggg
+g  gg
 `,
 ];
 
@@ -22,8 +22,8 @@ const G = {
   INIT_POS_X: 30,
   INIT_POS_Y: 80,
   SPAWN_SQUID_RATE: 60,
-  PARTICLE_VELOCITY_MIN: 0.1,
-  PARTICLE_VELOCITY_MAX: 3.0,
+  PARTICLE_VELOCITY_MIN: 0.5,
+  PARTICLE_VELOCITY_MAX: 1.0,
   STOP_POS_X: 150,
   STOP_POS_Y: 10,
 };
@@ -40,6 +40,11 @@ const G = {
  * @type  { Squid }
  */
 let player;
+
+/**
+ * @type  { Squid [] }
+ */
+let school = [];
 
 /**
  * @type  { Squid [] }
@@ -73,19 +78,20 @@ function update() {
   if (timer <= 0) {
     timer = 1000;
   }
-  
+
+
   if (input.isJustPressed) {
     verticalAcceleration = 0.5 * player.velocity;
     horizontalAcceleration = -0.5 * player.velocity;
   }
-  
-  if(input.isJustReleased){
+
+  if (input.isJustReleased) {
     verticalAcceleration = -0.5 * player.velocity;
     horizontalAcceleration = 1.5 * player.velocity;
   }
 
-  color("blue");
-  box(0, 30, 600, 1);
+
+
   player.pos.x = clamp(
     player.pos.x + verticalAcceleration,
     G.INIT_POS_X,
@@ -100,40 +106,84 @@ function update() {
   // console.log("Postion", player.pos);
   char("a", player.pos);
 
+
+
   spawnSquid();
   moveSquid();
 
- 
+
   handleCollision();
+  updateSchool();
+  color("blue");
+  box(0, 30, 600, 1);
 }
 
 function spawnSquid() {
   squidSpawn--;
-  if(squidSpawn > 0) return;
+  if (squidSpawn > 0) return;
   const y = rnd(30, 80);
   /**
   * @type  { Squid }
   */
   const squid = {
-      pos: vec(300, y),
-      velocity: G.PARTICLE_VELOCITY_MIN,
-      acceleration: 0,
+    pos: vec(300, y),
+    velocity: G.PARTICLE_VELOCITY_MIN,
+    acceleration: 0,
   };
   squids.push(squid);
   squidSpawn = G.SPAWN_SQUID_RATE;
 }
 
-function handleCollision() {
-  const isColidingWithSquids = char("a", player.pos).isColliding.char.b;
-  if(isColidingWithSquids){
-    console.log("Touching other squids");
-  }
-}
+
 function moveSquid() {
-  squids.forEach((squid)=>{
-    if(squid.pos.x < 0) squids.splice(squids.indexOf(squid), 1);
+  squids.forEach((squid) => {
+    const isOutofBounds = squid.pos.x < 0;
+    const isCollidingWithPlayer = char("b", squid.pos).isColliding.char.a;
+    if (isOutofBounds || isCollidingWithPlayer) squids.splice(squids.indexOf(squid), 1);
     squid.pos.x -= rnd(G.PARTICLE_VELOCITY_MIN, G.PARTICLE_VELOCITY_MAX);
+    color("black");
     char("b", squid.pos);
   })
 }
+
+
+function handleCollision() {
+  const isColidingWithSquids = char("a", player.pos).isColliding.char.b;
+  if (isColidingWithSquids) {
+    play("powerUp");
+    addScore(1);
+    arc(player.pos, 6, 1);
+    /**
+     ** @type  { Squid }
+     */
+    const squid = {
+      pos: (player.pos),
+      velocity: player.velocity,
+      acceleration: 0
+    }
+    school.push(squid);
+
+  }
+}
+function updateSchool() {
+  const angleIncrement = (2 * Math.PI) / school.length; // Angle between squids
+  let currentAngle = 0;
+
+  school.forEach((squid) => {
+    // Calculate the position relative to the player based on the angle
+    const offsetX = 20 * Math.cos(currentAngle); // Adjust the distance from the player
+    const offsetY = 20 * Math.sin(currentAngle);
+
+    // Set the squid's position to be around the player
+    squid.pos = vec(player.pos.x + offsetX, player.pos.y + offsetY);
+
+    // Update the angle for the next squid
+    currentAngle += angleIncrement;
+
+    // Draw the squid
+    color('blue');
+    char('b', squid.pos);
+  });
+}
+
 
