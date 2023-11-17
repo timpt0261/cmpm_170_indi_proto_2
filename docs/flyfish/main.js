9976e2f89e05dbb01023350ab7e136c49f19d2cd
timpt0261 <rchavez0215@gmail.com>
2023-11-16 18:21:57 -0800

    Squids are able to spawn

--- docs/flyfish/main.js
+++ docs/flyfish/main.js
@@ -9,6 +9,10 @@ characters = [
 b  bb
  b1bbb
 b  bb
+`,`
+r  rr
+ r1rrr
+r  rr
 `,
 ];
 
@@ -16,9 +20,10 @@ const G = {
   WIDTH: 300,
   HEIGHT: 100,
   INIT_POS_X: 30,
-  INIT_POS_Y: 50,
-  PARTICLE_VELOCITY_MIN: 0.5,
-  PARTICLE_SPEED_MAX: 1.0,
+  INIT_POS_Y: 80,
+  SPAWN_SQUID_RATE: 60,
+  PARTICLE_VELOCITY_MIN: 0.1,
+  PARTICLE_VELOCITY_MAX: 3.0,
   STOP_POS_X: 150,
   STOP_POS_Y: 10,
 };
@@ -39,7 +44,7 @@ let player;
 /**
  * @type  { Squid [] }
  */
-let squids;
+let squids = [];
 
 options = {
   viewSize: { x: G.WIDTH, y: G.HEIGHT },
@@ -50,6 +55,7 @@ options = {
 
 let verticalAcceleration = 0;
 let horizontalAcceleration = 0;
+let squidSpawn = G.SPAWN_SQUID_RATE;
 
 let lines = [];
 let timer = 1000;
@@ -58,28 +64,28 @@ function update() {
   if (!ticks) {
     player = {
       pos: vec(G.INIT_POS_X, G.INIT_POS_Y),
-      velocity: G.PARTICLE_VELOCITY_MIN,
+      velocity: 0.8,
       acceleration: 0,
     };
+
   }
   timer--;
   if (timer <= 0) {
     timer = 1000;
   }
-  // if (timer % 100 === 0) spawnSquid();
   
-  
-
-  console.log("timer", timer);
-  if (input.isPressed) {
+  if (input.isJustPressed) {
     verticalAcceleration = 0.5 * player.velocity;
     horizontalAcceleration = -0.5 * player.velocity;
-  } else {
-    
+  }
+  
+  if(input.isJustReleased){
     verticalAcceleration = -0.5 * player.velocity;
-    horizontalAcceleration = player.pos.y > 15 ? 1.5 * player.velocity : - 1.5 * player.velocity;
+    horizontalAcceleration = 1.5 * player.velocity;
   }
 
+  color("blue");
+  box(0, 30, 600, 1);
   player.pos.x = clamp(
     player.pos.x + verticalAcceleration,
     G.INIT_POS_X,
@@ -91,40 +97,43 @@ function update() {
     G.STOP_POS_Y,
     G.INIT_POS_Y
   );
-  console.log("Postion", player.pos);
+  // console.log("Postion", player.pos);
   char("a", player.pos);
 
+  spawnSquid();
+  moveSquid();
 
-  // if(squids.length > 0 ){
-  //    squids.forEach((squid) => {
-  //      char("a", squid.pos);
-  //    });
-
-  // }
  
   handleCollision();
 }
 
 function spawnSquid() {
-  const x = rnd(G.INIT_POS_X + 50, G.STOP_POS_X);
-  const y = rnd(G.STOP_POS_Y, G.INIT_POS_Y);
-  const amount = rnd(1, 3);
-  const displace = { x, y };
-  for (let i = 0; i <= amount; i++) {
-    displace.x += rnd(-1, 1);
-    displace.y += rnd(-1, 1);
-    /**
-     * @type  { Squid }
-     */
-    const squid = {
-      pos: vec(displace),
-      velocity: G.PARTICLE_SPEED_MAX,
+  squidSpawn--;
+  if(squidSpawn > 0) return;
+  const y = rnd(30, 80);
+  /**
+  * @type  { Squid }
+  */
+  const squid = {
+      pos: vec(300, y),
+      velocity: G.PARTICLE_VELOCITY_MIN,
       acceleration: 0,
-    };
-    squids.push(squid);
-  }
+  };
+  squids.push(squid);
+  squidSpawn = G.SPAWN_SQUID_RATE;
 }
 
 function handleCollision() {
-  // Add collision handling logic here
+  const isColidingWithSquids = char("a", player.pos).isColliding.char.b;
+  if(isColidingWithSquids){
+    console.log("Touching other squids");
+  }
 }
+function moveSquid() {
+  squids.forEach((squid)=>{
+    if(squid.pos.x < 0) squids.splice(squids.indexOf(squid), 1);
+    squid.pos.x -= rnd(G.PARTICLE_VELOCITY_MIN, G.PARTICLE_VELOCITY_MAX);
+    char("b", squid.pos);
+  })
+}
+
